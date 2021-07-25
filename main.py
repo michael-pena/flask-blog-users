45371950159b1795193d4c14f9eb045b59974214
mpena <michael.pena@protonmail.ch>
2021-07-24 21:58:53 -0500

    added the psycopg2-binary for PostgreSQL and added the import OS module to import the secret key and database environment variables from Heroku

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from flask_bcrypt import Bcrypt
 from functools import wraps
+import os
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.enviorn.get("DATABASE_URL")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -21,7 +22,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.enviorn.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
